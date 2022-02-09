c39c4f392c3594be490336823675984c064933cc
Petar Petrov <petar.dragov.petrov@gmail.com>
2022-02-09 15:36:09 +0100

    CHORE:Added a new solution

---
+++ characters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
