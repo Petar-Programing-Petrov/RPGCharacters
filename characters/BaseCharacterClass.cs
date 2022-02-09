5bd0a95e094dbe98643159e688d6b79f8f5b1e5e
Petar Petrov <petar.dragov.petrov@gmail.com>
2022-02-09 22:12:19 +0100

    CHORE: Added character base class

---
+++ characters/BaseCharacterClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace characters
+{
+    internal class BaseCharacterClass
+    {
+        private string characterName;
+        private string characterClass;
+        private string equipment;
+        private string armor;
+
+        //Stats primary atributes
+        private int strength;
+        private int dexterity;
+        private int intelligence;
+    }
+}
