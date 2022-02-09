66130d23bd7e1ea50bee23be6c7aa9a7fb7576fb
Petar Petrov <petar.dragov.petrov@gmail.com>
2022-02-09 22:52:57 +0100

    FEAT: Added base class for character types inheriting from the base CharacterClass

--- characters/BaseCharacterClass.cs
+++ characters/BaseCharacterClass.cs
@@ -17,5 +17,44 @@ namespace characters
         private int strength;
         private int dexterity;
         private int intelligence;
+
+        public string CharacterName
+        {
+            get { return characterName; }
+            set { characterName = value; }
+
+        }
+        public string CharacterClass
+        {
+            get { return characterClass; }
+            set { characterClass = value; }
+
+        }
+        public string Equipment
+        {
+            get { return equipment; }
+            set { equipment = value; }
+
+        }
+        public string Armor
+        {
+            get { return armor; }
+            set { armor = value; }
+        }
+        public int Strength
+        {
+            get { return strength; }
+            set { strength = value; }
+        }
+        public int Dexterity
+        {
+            get { return dexterity; }
+            set { dexterity = value; }
+        }
+        public int Intelligence
+        {
+            get { return intelligence; }
+            set { intelligence = value; }
+        }
     }
 }
---
+++ characters/BaseMageClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace characters
+{
+    internal class BaseMageClass : BaseCharacterClass
+    {
+        public void MageClass()
+        {
+            CharacterClass = "Mage";
+        }
+    }
+}
