8b541600f916c8fe2ff16e99d221162046324c53
Petar Petrov <petar.dragov.petrov@gmail.com>
2022-02-09 15:39:26 +0100

    CHORE: Added a new variable

--- characters/Program.cs
+++ characters/Program.cs
@@ -7,6 +7,7 @@ namespace characters
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string characterName = "Pesho";
         }
     }
 }
